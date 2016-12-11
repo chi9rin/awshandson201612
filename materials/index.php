d0a4cd4430253e2f3a905b3105537c43cf979e7a
Shinya TAKEBAYASHI <omoikanenomikoto@gmail.com>
2016-12-12 08:38:39 +0900

    add first version

---
+++ materials/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$server_ip = $_SERVER['SERVER_ADDR'];
+
+$dbhost = '<< RDS Endpoint >>';
+$dbport = '3306';
+$dbname = '<< DB Name >>';
+
+$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
+$username = '<< Username >>';
+$password = '<< Password >>';
+
+try {
+  $dbh = new PDO($dsn, $username, $password);
+  $UPDATE_COUNTER_QUERY = 'UPDATE data SET value = value + 1 WHERE name = "counter"';
+  $GET_COUNTER_QUERY = 'SELECT value FROM data WHERE name = "counter"';
+
+  $dbh->query($UPDATE_COUNTER_QUERY);
+  foreach($dbh->query($GET_COUNTER_QUERY) as $row) {
+    $counter_value = $row['value'];
+  }
+} catch (PDOException $e) {
+  print('cannot connect to mysql ep' + $dbhost);
+}
+?>
+
+<html>
+  <head>
+    <title>AWS handson</title>
+  </head>
+  <body>
+    <p>Connected: <? print($server_ip) ?></p>
+    <p>Counter: <? print($counter_value) ?></p>
+  </body>
+</html>
+
